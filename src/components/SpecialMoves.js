6fcb72f25e6659a850822cea851635c147d90998
Manuel <mosquera.manuel2011@gmail.com>
2021-03-26 12:16:25 -0500

    minor bug fixed

--- src/components/SpecialMoves.js
+++ src/components/SpecialMoves.js
@@ -25,8 +25,8 @@ const SpecialMoves = ({ windowState, changeWindowState }) => {
                     This special rule is called “en passant,” which is French for “in passing”. If a pawn moves out two squares on its first move, and by doing so lands to the side of an opponent's pawn (effectively jumping past the other pawn's ability to capture it), that other pawn has the option of capturing the first pawn as it passes by.
                     </p>
                     <div className="special-section__imgs-box">
-                        <img className="special-section__img" src="./img/enPassant1.jpg" alt="En Passant first move"/>
-                        <img className="special-section__img" src="./img/enPassant2.jpg" alt="En Passant second move"/>
+                        <img className="special-section__img" src="./enPassant1.jpg" alt="En Passant first move"/>
+                        <img className="special-section__img" src="./enPassant2.jpg" alt="En Passant second move"/>
                     </div>
                     <p className="special-section__text">
                         To make this movement chose the pawn that is going to make the "en passant move", and select its final location.
@@ -55,8 +55,8 @@ const SpecialMoves = ({ windowState, changeWindowState }) => {
                         </ul>
                     </div>
                     <div className="special-section__imgs-box">
-                        <img className="special-section__img" src="./img/castling1.jpg" alt="Castling first move"/>
-                        <img className="special-section__img" src="./img/castling2.jpg" alt="Castling second move"/>
+                        <img className="special-section__img" src="./castling1.jpg" alt="Castling first move"/>
+                        <img className="special-section__img" src="./castling2.jpg" alt="Castling second move"/>
                     </div>
                     <p className="special-section__text">
                     To make this move you must select the king first and then select the rook which you want to make the move.
@@ -69,8 +69,8 @@ const SpecialMoves = ({ windowState, changeWindowState }) => {
                     Pawns have another special ability and that is that if a pawn reaches the other side of the board it can become any other chess piece (called promotion) excluding a king (or pawn, for that matter).
                     </p>
                     <div className="special-section__imgs-box">
-                        <img className="special-section__img" src="./img/promotePawn.jpg" alt="Promote pawn first move"/>
-                        <img className="special-section__img" src="./img/promotePawn2.jpg" alt="Promote pawn second move"/>
+                        <img className="special-section__img" src="./promotePawn.jpg" alt="Promote pawn first move"/>
+                        <img className="special-section__img" src="./promotePawn2.jpg" alt="Promote pawn second move"/>
                     </div>
                     <p className="special-section__text">
                     To make this move just click in the "Promote" button that will appear when you reaches the other side and select the piece you want to replace the pawn. The pawn must be promoted as soon as it reaches the other side or the opportunity to promote it will be lost.
